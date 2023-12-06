662ef829a90be4be675e5793ecd7e10d9335172f
borisbonchev <b.bonchev@student.fontys.nl>
2023-12-06 13:14:13 +0100

    dinglemouse

---
+++ FIXME_static_electrickery/Dinglemouse.java
@@ -0,0 +1,18 @@
+package FIXME_static_electrickery;
+
+public class Dinglemouse {
+
+  private static int ONE_HUNDRED = 100; // Declare this first!
+  
+  public static final Dinglemouse INST = new Dinglemouse();
+
+  private final int value; 
+
+  private Dinglemouse() {
+    value = ONE_HUNDRED;
+  }
+  
+  public int plus100(int n) {
+    return value + n;
+  }
+}
---
+++ FIXME_static_electrickery/ExampleTests.java
@@ -0,0 +1,14 @@
+package FIXME_static_electrickery;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTests {
+    
+  @Test
+  void test() {
+    assertEquals(123, Dinglemouse.INST.plus100(23));
+    // Err... was that right?
+  }
+    
+}
